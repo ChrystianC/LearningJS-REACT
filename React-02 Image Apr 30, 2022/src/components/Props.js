70008e50163e76126895b1a719028b1f94a5562c
ChrystianC <91797794+ChrystianC@users.noreply.github.com>
2022-12-23 02:00:53 +0100

    Delete React-02 Image Apr 30, 2022/src directory

--- React-02 Image Apr 30, 2022/src/components/Props.js
+++
@@ -1,2 +0,0 @@
-const props = [{ Img: "pexels-merve-şahin-11161043.jpg", Rating: 0.1, ReviewPost: 48, Country: "France", Title: "Life lessons with Katie Zaferes", Cost: 150 }];
-export default props;
